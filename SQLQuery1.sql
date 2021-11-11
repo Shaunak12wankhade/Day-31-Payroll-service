c0813e7a1ba6dcf7e03ce84be3c43f7939f1d284
unknown <shaunak12wankhade@ggmail.com>
2021-11-11 12:42:11 +0530

    [Shaunak] Added : Employee Payroll table in payroll service database

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -4,3 +4,17 @@ Welcome to payroll service
 -------Create DataBase------
 CREATE DATABASE payroll_service;
 USE payroll_service;
+
+
+--UC2--
+---Create table for DataBase---
+CREATE TABLE employee_payroll(
+id int not null,
+name varchar(20) not null,
+salary float not null,
+startdate date not null
+);
+
+
+
+
